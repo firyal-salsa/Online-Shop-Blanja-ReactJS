8a3b5177a21f61adaefd099e8348e3c2bb45fbe0
firyal-salsa <firsabilacahyadi@gmail.com>
2021-10-06 22:03:15 +0700

    add a sign up page

--- src/components/signupcustomer/index.jsx
+++ src/components/signupcustomer/index.jsx
@@ -1,51 +1,77 @@
-import axios from 'axios'
-import React from 'react'
-import { Component } from 'react'
-import "./style/style.scoped.css"
+import axios from "axios";
+import React from "react";
+import {useState} from "react";
+import { useHistory } from "react-router-dom";
+import "./style/style.scoped.css";
 
-class SignUpCS extends Component {
-    constructor(props) {
-        super(props)
 
-        this.state = {
-            login: false,
-            type: "customers",
-            customers: {
-                email: "",
-                name: "",
-                password: "",
-                confirm: "",
-            },
-        }
-    }
+function SignUpCS(){
+  const [data,setData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  })
+  const history = useHistory();
 
-    signup = () => {
-            axios({
-                url: `${process.env.REACT_APP_API}/customer`,
-                method: "POST",
-                data: this.state.customers,
-            })
-                .then((res) => {
-                    this.props.history.push("/login")
-                })
-                .catch((error) => {
-                    console.log(error)
-                })
+  function submit(e){
+    e.preventDefault();
+    axios.post(`${process.env.REACT_APP_API}/customer`, {
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    })
+    .then(res=>{
+      console.log(res.data)
+      history.push("/login");
+    })
+    .catch(err => console.log(err))
+  }
+  function handle(e){
+    const newdata={...data}
+    newdata[e.target.id] = e.target.value
+    setData(newdata)
+    console.log(newdata)
+  }
+  return(
+    <div className="d-flex flex-column d-flex justify-content-center">
+      <form onSubmit={(e) => submit(e)} className="signup text-center">
+          <input
+            type="text"
+            className="signup-input p-2"
+            value={data.name}
+            onChange={(e) => handle(e)}
+            id="name"
+            placeholder="Name"
+            required
+          />
+          <input
+            type="email"
+            className="signup-input p-2"
+            value={data.email}
+            onChange={(e) => handle(e)}
+            id="email"
+            placeholder="Email"
+            required
+          />
+          <input
+            type="password"
+            className="signup-input p-2"
+            value={data.password}
+            onChange={(e) => handle(e)}
+            id="password"
+            placeholder="Password"
+            required
+          />
+          <button
+            type="submit"
+            className="btn button-primary text-center mt-5"
+          >
+            SIGN UP
+          </button>
+      </form>
+    </div>
+  );
 }
 
 
-    render() {
-        return (
-            <div className="d-flex flex-column d-flex justify-content-center">
-            <main className="signup text-center">
-                <input type="text" className="signup-input p-2" name="name" placeholder="Name" value={this.state.customers.name} required/>
-                <input type="email" className="signup-input p-2" name="email" placeholder="Email" value={this.state.customers.email}/>
-                <input type="password" class="signup-input p-2" name="password"  placeholder="Password" autoComplete="off" value={this.state.customers.password}/>
-                <button type="submit" name="signup" onClick={this.signup} ref={this.refRegisButton} className="btn button-primary text-center mt-5">PRIMARY</button>
-            </main>
-        </div>
-        )
-    }
-}
-
-export default SignUpCS
+export default SignUpCS;
--- src/components/signupseller/index.jsx
+++ src/components/signupseller/index.jsx
@@ -1,19 +1,96 @@
-import React from 'react'
-import "./style/style.scoped.css"
-import { Link } from 'react-router-dom'
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import "./style/style.scoped.css";
 
-export default function index() {
-    return (
-        <div className="d-flex flex-column d-flex justify-content-center">
-            <main className="signup text-center">
-                <input type="text" className="signup-input p-2" name="" placeholder="Name" />
-                <input type="email" className="signup-input p-2" name="" placeholder="Email" />
-                <input type="text" className="signup-input p-2" name="" placeholder="Phone Number" />
-                <input type="text" className="signup-input p-2" name="" placeholder="Store Name" />
-                <input type="password" class="signup-input p-2" name=""  placeholder="Password" />
-                <Link to="/home"  className="btn button-primary text-center mt-5">PRIMARY</Link>
-            </main>
-        </div>
-       
-    )
+function SignUpSL() {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    phone_number: "",
+    store_name: "",
+    password: "",
+  });
+  const history = useHistory();
+
+  function submit(e) {
+    e.preventDefault();
+    axios
+      .post(`${process.env.REACT_APP_API}/seller`, {
+        name: data.name,
+        email: data.email,
+        phone_number: data.phone_number,
+        store_name: data.store_name,
+        password: data.password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        history.push("/login");
+      })
+      .catch((err) => console.log(err));
+  }
+  function handle(e) {
+    const newdata = { ...data };
+    newdata[e.target.id] = e.target.value;
+    setData(newdata);
+    console.log(newdata);
+  }
+
+  return (
+    <div className="d-flex flex-column d-flex justify-content-center">
+      <form onSubmit={(e) => submit(e)} className="signup text-center">
+        <input
+          type="text"
+          className="signup-input p-2"
+          value={data.name}
+          onChange={(e) => handle(e)}
+          id="name"
+          placeholder="Name"
+          required
+        />
+        <input
+          type="email"
+          className="signup-input p-2"
+          value={data.email}
+          onChange={(e) => handle(e)}
+          id="email"
+          placeholder="Email"
+          required
+        />
+        <input
+          type="text"
+          className="signup-input p-2"
+          value={data.phone_number}
+          onChange={(e) => handle(e)}
+          id="phone_number"
+          placeholder="Phone Number"
+          required
+        />
+        <input
+          type="text"
+          className="signup-input p-2"
+          value={data.store_name}
+          onChange={(e) => handle(e)}
+          id="store_name"
+          placeholder="Store Name"
+          required
+        />
+        <input
+          type="password"
+          className="signup-input p-2"
+          value={data.password}
+          onChange={(e) => handle(e)}
+          placeholder="Password"
+          id="password"
+          required
+        />
+        <button type="submit" className="btn button-primary text-center mt-5">
+          SIGN UP
+        </button>
+      </form>
+    </div>
+  );
 }
+
+export default SignUpSL;
