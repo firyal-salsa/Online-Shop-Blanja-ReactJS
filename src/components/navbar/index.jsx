be1ee1c7a24e332cf1494346ede184e8ab2a1cde
firyal-salsa <53255114+firyal-salsa@users.noreply.github.com>
2021-11-03 14:21:21 +0700

    navvv

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import "./style/style.css"
+import "./style/style.scoped.css"
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Link } from 'react-router-dom';
 
@@ -7,8 +7,8 @@ class Navbar extends Component {
     render() {
         return (
             <div id="cart-button" className="p-3 ms-5">
-                <Link to="/login" type="button" id="navbar-red" className="btn px-5 m-1 rounded-pill">Login</Link>
-                <Link to="/signup" type="button" className="btn btn-outline-secondary px-5 m-1 rounded-pill">Sign Up</Link>
+                <Link to="/login" type="button"className="btn btn-primary btns px-5 m-1 rounded-pill">Login</Link>
+                <Link to="/signup" type="button" className="btn btn-outline-secondary btns px-5 m-1 rounded-pill">Sign Up</Link>
             </div>
         )
     }
